431a0c17c7cb3cb210c2d8a48e4bb0621d64c00b
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-07-01 14:14:17 +0000

    Change name second time in Allowed Hosts

--- celiacs_api/settings.py
+++ celiacs_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-sofianords-celiacsapi-zltkle6gtsu.ws.codeinstitute-ide.net', 'celiacs-api.herokuapp.com']
+ALLOWED_HOSTS = ['8000-sofianords-celiacsapi-zltkle6gtsu.ws.codeinstitute-ide.net', 'celiacs-api-bf52b941b62a.herokuapp.com/']
 
 # Application definition
 
