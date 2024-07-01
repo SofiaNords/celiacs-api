8d402f4c54be9666fdee265dab7700d04325ec62
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-07-01 05:52:09 +0000

    Add search filters to PostList

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Generic Views where used as a shortcut for common usage patterns.
 
 - [Cloudinary](https://cloudinary.com/) - Used to store images
 - [Django](https://www.djangoproject.com/) - Used for rapid, reusable and secure development
+- [Django-Filter](https://django-filter.readthedocs.io/en/stable/) - Used to add filters in search feature
 - [Django REST Framework](https://www.django-rest-framework.org/) - Used for buildning Web API
 - [Git](https://git-scm.com/) - Used for version control
 - [GitHub](https://github.com/) - Used as the IDE for development
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from celiacs_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -19,6 +20,11 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'select__owner__profile',
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
