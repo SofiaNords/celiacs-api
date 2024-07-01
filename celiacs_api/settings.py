374835047902f0206d7222e0e2596ec44a7a6a3e
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-07-01 15:36:06 +0000

    Add os.environ.get to Allowed Hosts

--- celiacs_api/settings.py
+++ celiacs_api/settings.py
@@ -61,7 +61,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-sofianords-celiacsapi-zltkle6gtsu.ws.codeinstitute-ide.net', 'celiacs-api-bf52b941b62a.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-sofianords-celiacsapi-zltkle6gtsu.ws.codeinstitute-ide.net',
+    os.environ.get('ALLOWED_HOST'),
+]
 
 # Application definition
 
