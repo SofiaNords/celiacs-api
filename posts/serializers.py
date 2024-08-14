ac028c61b0065b4c9df842abf487e0313ab76fc2
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-08-14 13:49:15 +0000

    Add score-display to PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -26,6 +26,9 @@ class PostSerializer(serializers.ModelSerializer):
     # Read-only field to display the count of selects on the post
     select_count = serializers.ReadOnlyField()
 
+    # Field to display the full description of the score
+    score_display = serializers.SerializerMethodField()
+
     # Method to validate the image field
     def validate_image(self, value):
         # Check if the image size is larger than 2MB
@@ -66,5 +69,5 @@ class PostSerializer(serializers.ModelSerializer):
             'profile_image', 'created_at', 'updated_at',
             'title', 'location', 'content', 'image',
             'select_id', 'comments_count', 'select_count',
-            'score'
+            'score', 'score_display',
         ]
