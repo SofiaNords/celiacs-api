5a80c6430d511437cedb45f01fd2d71f0de5774a
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-08-15 08:51:07 +0000

    Create category views

--- category/views.py
+++ category/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Category
+from .serializers import CategorySerializer
+from celiacs_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+# View for listing and creating categories
+class CategoryList(generics.ListCreateAPIView):
+    # Specify the serializer class to use
+    serializer_class = CategorySerializer
+
+    # Set the permission classes for the view
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # Specify the filter backends to use for filtering, searching, and ordering
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    # Define the fields that can be used for filtering
+    filterset_fields = [
+        'name',
+    ]
+
+# View for retrieving, updating and deleting a specific category
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    # Specify the serialixer class to use
+    serializer_class = CategorySerializer
+
+    # Set the permission classes for the view
+    permission_classes = [IsOwnerOrReadOnly]
+
+    # Define the queryset
+    queryset = Category.objects.all()
