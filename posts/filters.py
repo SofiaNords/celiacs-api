f4f6dcb5412b49d48ebe1713c6cd7dd15ea946c9
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-08-17 08:46:04 +0000

    Remove filters.py in posts app

--- posts/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import Post
-
-class PostFilter(django_filters.FilterSet):
-    score = django_filters.CharFilter(field_name='score', lookup_expr='icontains')
-
-    class Meta:
-        model = Post
-        fields = ['score']
